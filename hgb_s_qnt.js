41375d13898c474fb36cd8c956a876759baa6bd3
brettmrice <brettmrice@gmail.com>
2021-05-26 20:14:57 -0400

    Update hgb_s_qnt.js

--- hgb_s_qnt.js
+++ hgb_s_qnt.js
@@ -27,13 +27,15 @@ function setup() {
   valueStart = inputArea_startY + inputArea_endY/4 + valueInt;
   
   totalInput = [0, 0, 0];
-  totalDisplay = 'Click!';
+  //totalDisplay = 'Click!';
+  totalDisplay = '';
   totalClick = 0;
   totalKeyCount = 0;
   totalSum = 0;
   
   hgbsInput = [0, 0, 0];
-  hgbsDisplay = 'Click!';
+  //hgbsDisplay = 'Click!';
+  hgbsDisplay = '';
   hgbsClick = 0;
   hgbsKeyCount = 0;
   hgbsSum = 0;
